f0ceb0654baf8ff3cb77d302154655ad9be620a1
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2019-03-13 09:53:47 +0100

    fix newsletter module

---
+++ Model/Queue/NewsletterTransportBuilder.php
@@ -0,0 +1,25 @@
+<?php
+namespace MageSuite\EmailAttachments\Model\Queue;
+
+class NewsletterTransportBuilder extends \Magento\Newsletter\Model\Queue\TransportBuilder
+{
+    /**
+     * @inheritdoc
+     */
+    protected function prepareMessage()
+    {
+        /** @var \Magento\Email\Model\AbstractTemplate $template */
+        $template = $this->getTemplate()->setData($this->templateData);
+        $this->setTemplateFilter($template);
+
+        $this->message->setBodyHtml(
+            $template->getProcessedTemplate($this->templateVars)
+        )->setSubject(
+            $template->getSubject()
+        );
+        
+        $this->message->setPartsToBody();
+
+        return $this;
+    }
+}
